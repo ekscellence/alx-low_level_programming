f43435349ab57030b8650a43770c66bc5039d5c0
ekscellence <ekscellence@yahoo.com>
2023-03-29 04:32:30 -0700

    nnnn

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
