2fae1e6859ed138e5c918492649004b6f4dd0506
ekscellence <ekscellence@yahoo.com>
2023-03-29 04:27:00 -0700

    nnn

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * _isalpha - Shows 1 if the input is a
+ * letter Another cases, shows 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for letters. 0 for the rest.
+ */
 
 int _isalpha(int c)
 {
